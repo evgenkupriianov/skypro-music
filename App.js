0b95c8d3029b0d88f11b6d5552dabc2ed952e140
evgenkupriianov <kupriianovi@inbox.ru>
2024-01-07 18:03:26 +0300

    4

---
+++ App.js
@@ -0,0 +1,22 @@
+import "./styles/style.css";
+import CenterBlock from "./components/CenterBlock/CenterBlock";
+import Sidebar from "./components/sidebar/sidebar";
+import MusicPlayer from "./components/musicPlayer/musicPlayer";
+import MainNav from "./components/mainNav/mainNav";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <main className="main">
+          <MainNav />
+          <CenterBlock />
+          <Sidebar />
+          <MusicPlayer />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
